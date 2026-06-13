5c6abc4ec9ff6b5fa2d0b0f8f6918b1940f5e9b4
floydx3077 <floydx3077@student.faytechcc.edu>
2026-06-13 16:59:52 -0400

    Lab upload 1

---
+++ P1LAB1_FloydXavier.py
@@ -0,0 +1,11 @@
+# Xavier Floyd
+# 6/13/2026
+# P1LAB1
+# A program that takes a user's first and last names through separate inputs and displays a welcome message with the user's name for the course
+
+# Prompt the user for their names and store them in separate variables
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display the welcome message
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110.")
