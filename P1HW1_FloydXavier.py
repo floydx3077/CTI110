9217bf46ec20016c61b1d151dd9ec9440ba5bf3b
floydx3077 <floydx3077@student.faytechcc.edu>
2026-06-13 20:49:52 -0400

    Homework 1 Upload

---
+++ P1HW1_FloydXavier.py
@@ -0,0 +1,26 @@
+# Xavier Floyd
+# 6/13/2026
+# P1HW1
+# A program that takes user input of numerical values and uses them in mathematical expressions
+
+# Caculate Exponents
+print("------------------Exponents-----------------\n")
+
+# User input
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print("\n")
+
+print(base, "raised to the power of", exponent, "is", base**exponent, "!!")
+print("\n")
+
+# Caculate Exponents
+print("----------Addition and Subtraction----------\n")
+
+# User input
+start_num = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print("\n")
+
+print(start_num, "+", add, "-", subtract, "is equal to", start_num + add - subtract)
